41e8c0d98ddd882aeed96a0762685758e2d79555
Bchiru045 <chiranjitbera811@gmail.com>
2025-12-09 12:00:39 +0530

    After fixing the all errors of netlify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -10,13 +10,13 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
             <div className="social-icon">
-              <a href="#">
+              <a href="javascript:void(0);">
                 <img src={navIcon1} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="javascript:void(0);">
                 <img src={navIcon2} alt="Icon" />
               </a>
-              <a href="#">
+              <a href="javascript:void(0);">
                 <img src={navIcon3} alt="Icon" />
               </a>
             </div>
